c8097c1cc8452f197c2239f4fde9bc90d5544ceb
Joe Bell <emailjoebell@gmail.com>
2020-11-03 11:43:37 -0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# bash script to install additional tools regularily needed for new ubuntu installs or VM's
+
+# installs visual studio code
+sudo apt update -y
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update -y
+sudo apt install code -y
+# installs google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+# clean-up
+rm ./google-chrome-stable_current_amd64.deb
+# install git & gitk
+sudo apt install git -y
+sudo apt install gitk -y
