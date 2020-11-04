954f5429cc9fc539a0ac94a14b99cc4c8a13c0df
joe bell <joseph.bell.17@us.af.mil>
2020-11-04 16:04:06 -0700

    added git-gui to feat3

--- install.sh
+++ install.sh
@@ -16,11 +16,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 # clean-up google chrome
 rm ./google-chrome-stable_current_amd64.deb
-# install git & gitk
+# install git & gitk & git-gui
 sudo apt install git -y
 sudo apt install gitk -y
+sudo apt install git-gui -y
 # installing terminator terminal
 sudo apt install terminator -y
 # installing zoom
 sudo apt install ./zoom_amd64.deb -y
+
 #Adding additional comment for version testing
